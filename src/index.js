26588fb6a36eead3169a45f064bfb304c7e365e2
thatblondecodergirl <madeleine.alexandersson@gmail.com>
2022-12-06 14:49:09 +0000

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import WeatherSearch from "./WeatherSearch";
 
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -10,16 +11,21 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <div className="App">
-      <h1>Weather App</h1>
-      <WeatherSearch />
-      <footer>
-        <p>
-          <a href="https://github.com/thatblondecodergirl/weather-react">
-            Open-Source Code
-          </a>{" "}
-          by Madeleine Alexandersson{" "}
-        </p>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <WeatherSearch />
+        <footer>
+          <p>
+            <a
+              href="https://github.com/thatblondecodergirl/weather-react"
+              target="_blank"
+            >
+              Open-Source Code
+            </a>{" "}
+            by Madeleine Alexandersson{" "}
+          </p>
+        </footer>
+      </div>
     </div>
   </StrictMode>
 );
