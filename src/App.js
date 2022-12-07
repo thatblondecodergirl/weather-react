49b17c798ae6b719bd53e47a91d2640cb1c2492b
thatblondecodergirl <madeleine.alexandersson@gmail.com>
2022-12-07 10:53:24 +0000

    Updated component structure and added icons

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/madeleinealexandersson/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Madeleine Alexandersson
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/thatblondecodergirl/weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://resonant-marshmallow-0def52.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,31 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import WeatherSearch from "./WeatherSearch";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <div className="App">
-      <div className="container Search">
-        <h1>Weather App</h1>
-        <WeatherSearch />
-      </div>
-      <footer>
-        <p>
-          <a
-            href="https://github.com/thatblondecodergirl/weather-react"
-            target="_blank"
-          >
-            Open-Source Code
-          </a>{" "}
-          by Madeleine Alexandersson{" "}
-        </p>
-      </footer>
-    </div>
+    <App />
   </StrictMode>
 );
